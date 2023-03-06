476464b2104234c8d1f1a127f7604bd47e67263b
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-03-06 15:57:03 +0100

    Update Bitmap.ts

--- Bitmap.ts
+++ Bitmap.ts
@@ -38,8 +38,14 @@ const headerType : HeaderType[] = [
     }
 ];
 
+
+
 const decoder = new TextDecoder();
 
+/**
+ *
+ * @param array
+ */
 function uInt8ArrayToNumber( array : Uint8Array ) : number {
     let currentPower = 1;
     return array.reduce( ( previousValue, currentValue ) => {
@@ -50,10 +56,13 @@ function uInt8ArrayToNumber( array : Uint8Array ) : number {
 
 }
 
-
 export class Bitmap {
     private bitmap : Uint8Array;
 
+    /**
+     * 
+     * @param bitmap
+     */
     constructor( bitmap : Uint8Array ) {
         this.bitmap = bitmap;
     }
@@ -79,7 +88,7 @@ export class Bitmap {
     }
 
     public get size() : HeaderType | undefined {
-        return headerType.find( o => o.size === uInt8ArrayToNumber( this.bitmap.subarray( 14, 18 ) ) );
+        return headerType.find( type => type.size === uInt8ArrayToNumber( this.bitmap.subarray( 14, 18 ) ) );
     }
 
     public get width() : number {
@@ -97,5 +106,32 @@ export class Bitmap {
     public get bitsPerPixel() : number {
         return uInt8ArrayToNumber( this.bitmap.subarray( 28, 30 ) );
     }
+
+    public get compression() : number {
+        return uInt8ArrayToNumber( this.bitmap.subarray( 30, 34 ) );
+    }
+
+    public get imageSize() : number {
+        return uInt8ArrayToNumber( this.bitmap.subarray( 34, 38 ) );
+    }
+
+    public get xPixelsPerMeter() : number {
+        return uInt8ArrayToNumber( this.bitmap.subarray( 38, 42 ) );
+    }
+
+    public get yPixelsPerMeter() : number {
+        return uInt8ArrayToNumber( this.bitmap.subarray( 42, 46 ) );
+    }
+
+    public get colorsUsed() : number {
+        return uInt8ArrayToNumber( this.bitmap.subarray( 46, 50 ) );
+    }
+
+    public get importantColors() : number {
+        return uInt8ArrayToNumber( this.bitmap.subarray( 50, 54 ) );
+    }
     
+    public getPixelData() : Uint8Array {
+        return this.bitmap.subarray( 14 + ( this.size?.size || 12 ) );
+    }
 }
